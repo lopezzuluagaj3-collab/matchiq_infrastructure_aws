b1d50cc5a96b58bee065669cc44321b6a2ba92ee
lopezzuluagaj3-collab <lopezzuluagaj3@gmail.com>
2026-08-24 17:26:14 -0500

    se crea CI/CD

--- versions.tf
+++ versions.tf
@@ -1,10 +1,22 @@
+# versions.tf
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.7.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 5.0"
     }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "sirius-terraform-state-022784797877"
+    key     = "cruster/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
